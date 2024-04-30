c8b1c86898523526552746b377cb141aa5045652
sajjadmt <momenitabar@gmail.com>
2024-04-30 12:13:15 +0330

    Dynamic Product Details

--- app/Http/Controllers/ProductDetailsController.php
+++ app/Http/Controllers/ProductDetailsController.php
@@ -2,20 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\ProductDetails;
 use App\Models\ProductList;
+use App\Models\SubCategory;
 use Illuminate\Http\Request;
 
 class ProductDetailsController extends Controller
 {
     public function ProductDetails(Request $request)
     {
-        $productDetails = ProductDetails::find($request->id)->toArray();
-        $product = ProductList::find($request->id)->toArray();
-        $items = [
+        $productDetails = ProductDetails::find($request->id);
+        $product = ProductList::find($request->id);
+        $category = Category::find($product->category_id)->only('category_name');
+        $subCategory = SubCategory::find($product->sub_category_id)->only('sub_category_name');
+        return [
             'Details' => $productDetails,
             'Product' => $product,
+            'Category' => $category,
+            'SubCategory' => $subCategory,
         ];
-        return $items;
     }
 }
